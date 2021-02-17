54947e9decf7a2e13b69b96c3c0992cbdc888f9e
jingyu-uom <45375036+y8j31@users.noreply.github.com>
2021-02-17 13:40:32 +1100

    Update run_gen_flow.sh

--- scripts/run_gen_flow.sh
+++ scripts/run_gen_flow.sh
@@ -1,28 +1,3 @@
-cp ../fabric/IO_1_bidirectional.vhdl ./
-cp ../fabric/IO_1_bidirectional_frame_config_pass.vhdl ./
-cp ../fabric/LUT4c_latch_config.vhdl ./
-cp ../fabric/LUT4c_frame_config.vhdl ./
-cp ../fabric/MUX8LUT_latch_config.vhdl ./
-cp ../fabric/MUX8LUT_frame_config.vhdl ./
-cp ../fabric/InPassOPA.vhdl ./
-cp ../fabric/InPass4_frame_config.vhdl ./
-cp ../fabric/InPassOPB.vhdl ./
-cp ../fabric/OutPassRES0.vhdl ./
-cp ../fabric/OutPassRES1.vhdl ./
-cp ../fabric/OutPassRES2.vhdl ./
-cp ../fabric/OutPass4_frame_config.vhdl ./
-cp ../fabric/RegFile_32x4.vhdl ./
-cp ../fabric/MULADD.vhdl ./
-cp ../fabric/my_lib.vhdl ./
-cp ../fabric/*.list ./
-cp ../fabric/CPU_IO_ConfigMem.csv ./
-cp ../fabric/DSP_bot_ConfigMem.csv ./
-cp ../fabric/DSP_top_ConfigMem.csv ./
-cp ../fabric/LUT4AB_ConfigMem.csv ./
-cp ../fabric/RegFile_ConfigMem.csv ./
-cp ../fabric/W_IO_ConfigMem.csv ./
-cp ../fabric/verilog_base/*.v ./
-
 #REM STEP 1 : Generate the switch matrix as an empty CSV file containing the adjacency matrix (gets populated in Step 2)
 python3 gen.py -GenTileSwitchMatrixCSV
 #REM STEP 2 : populate the switch matrix (adjacency matrix) entries

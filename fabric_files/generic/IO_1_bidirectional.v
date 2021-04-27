43d64c3d85db8a23df3f29390b9313e936c85374
jingyu-uom <45375036+y8j31@users.noreply.github.com>
2021-04-27 11:12:30 +1000

    Update IO_1_bidirectional.v

--- fabric_files/generic/IO_1_bidirectional.v
+++ fabric_files/generic/IO_1_bidirectional.v
@@ -12,9 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-//Library UNISIM;
-//use UNISIM.vcomponents.all;
-
 module IO_1_bidirectional (I, T, O, Q, PAD, UserCLK, MODE, CONFin, CONFout, CLK);
 	// parameter LUT_SIZE = 4;	
 	// Pin0

f7f16b1f6b3f559bfda961fed6ea00ab68f3b05d
jingyu-uom <45375036+y8j31@users.noreply.github.com>
2021-04-27 11:18:18 +1000

    Update LUT4c_frame_config.v

--- fabric_files/generic/LUT4c_frame_config.v
+++ fabric_files/generic/LUT4c_frame_config.v
@@ -1,4 +1,16 @@
-`include "models_pack.v"
+/* Copyright 2021 University of Manchester
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 module LUT4c_frame_config (I0, I1, I2, I3, O, Ci, Co, UserCLK, ConfigBits);
 	parameter NoConfigBits = 18 ; // has to be adjusted manually (we don't use an arithmetic parser for the value)

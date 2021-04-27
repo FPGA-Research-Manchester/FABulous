00b205c615ac47abb48c1b58e9e7b90b00688175
jingyu-uom <45375036+y8j31@users.noreply.github.com>
2021-04-27 11:23:11 +1000

    Update OutPass4_frame_config.vhdl

--- fabric_files/generic/OutPass4_frame_config.vhdl
+++ fabric_files/generic/OutPass4_frame_config.vhdl
@@ -1,10 +1,22 @@
+-- Copyright 2021 University of Manchester
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use work.my_package.all;
 
--- InPassFlop2 and OutPassFlop2 are the same except for changing which side I0,I1 or O0,O1 gets connected to the top entity
--- InPassFlop2 and OutPassFlop2 are the same except for changing which side I0,I1 or O0,O1 gets connected to the top entity
 -- InPassFlop2 and OutPassFlop2 are the same except for changing which side I0,I1 or O0,O1 gets connected to the top entity
 
 entity OutPass4_frame_config is

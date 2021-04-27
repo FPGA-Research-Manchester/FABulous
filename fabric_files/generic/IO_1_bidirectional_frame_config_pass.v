1a2f5e89372f8920bd42b97929239dc591208e9d
jingyu-uom <45375036+y8j31@users.noreply.github.com>
2021-04-27 11:12:12 +1000

    Update IO_1_bidirectional_frame_config_pass.v

--- fabric_files/generic/IO_1_bidirectional_frame_config_pass.v
+++ fabric_files/generic/IO_1_bidirectional_frame_config_pass.v
@@ -1,5 +1,16 @@
-//Library UNISIM;
-//use UNISIM.vcomponents.all;
+/* Copyright 2021 University of Manchester
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 module IO_1_bidirectional_frame_config_pass (I, T, O, Q, I_top, T_top, O_top, UserCLK, ConfigBits);
 	parameter NoConfigBits = 0; // has to be adjusted manually (we don't use an arithmetic parser for the value)

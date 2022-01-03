ae17027f28cf542dfd74f38a17cea3dc934305b1
jingyu-uom <45375036+y8j31@users.noreply.github.com>
2022-01-03 21:53:28 +1100

    Update run_fab_flow.sh

--- fabric_generator/run_fab_flow.sh
+++ fabric_generator/run_fab_flow.sh
@@ -40,14 +40,26 @@ python3 fabric_gen.py -GenTileVerilog
 #REM STEP 6 : generate the entire fabric (RTL)
 python3 fabric_gen.py -GenFabricHDL
 python3 fabric_gen.py -GenFabricVerilog
+rm N_term_DSP_ConfigMem.v
+rm N_term_RAM_IO_ConfigMem.v
+rm N_term_single_ConfigMem.v
+rm N_term_single2_ConfigMem.v
+rm S_term_DSP_ConfigMem.v
+rm S_term_RAM_IO_ConfigMem.v
+rm S_term_single_ConfigMem.v
+rm S_term_single2_ConfigMem.v
 
-#REM STEP 7 : generate the nextpnr model
+#REM STEP 7 : generate verilog top wrapper
+python3 fabulous_top_wrapper_temp/top_wrapper_generator_with_BRAM.py -c $1 -r $2
+rm tb_bitbang.vhd
+
+#REM STEP 8 : generate the nextpnr model
 python3 fabric_gen.py -GenNextpnrModel
 
-#REM STEP 8 : generate the VPR model
+#REM STEP 9 : generate the VPR model
 python3 fabric_gen.py -GenVPRModel
 
-#REM STRP 9 : generate the meta data list for FASM -> bitstream
+#REM STRP 10 : generate the meta data list for FASM -> bitstream
 python3 fabric_gen.py -GenBitstreamSpec npnroutput/meta_data.txt
 #GOTO END
 

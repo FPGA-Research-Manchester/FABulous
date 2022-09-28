080b063951232c9a17aff6cbcb028f6a7777bfc4
gatecat <gatecat@ds0.me>
2022-09-28 16:52:51 +0200

    icarus: Fixing some testbench bits
    Signed-off-by: gatecat <gatecat@ds0.me>

--- fabric_files/fabric_icarus_example/run_simulation.sh
+++ fabric_files/fabric_icarus_example/run_simulation.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -ex
-BITSTREAM=test_design/counter.bin
+DESIGN=counter
+BITSTREAM=test_design/${DESIGN}.bin
 VERILOG=../../fabric_generator/verilog_output
 MAX_BITBYTES=16384
 
-iverilog $VERILOG/* fabulous_tb.v -s fab_tb -o fab_tb.vvp
+iverilog $VERILOG/* test_design/${DESIGN}.v fabulous_tb.v -s fab_tb -o fab_tb.vvp
 python3 makehex.py $BITSTREAM $MAX_BITBYTES bitstream.hex
 vvp fab_tb.vvp

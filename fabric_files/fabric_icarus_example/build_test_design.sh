eddfdcf979d799acda984e1377ef02dc6b634b27
King Lok Chung <king.chung@student.manchester.ac.uk>
2022-10-24 10:28:02 +0100

    Reverse some minor changes to Icarus testing

--- fabric_files/fabric_icarus_example/build_test_design.sh
+++ fabric_files/fabric_icarus_example/build_test_design.sh
@@ -7,8 +7,7 @@ BIT_GEN=../../nextpnr/fabulous/fab_arch/bit_gen.py
 DESIGN=counter
 
 set -ex
-# yosys -qp "tcl $SYNTH_TCL 4 top_wrapper test_design/${DESIGN}.json" test_design/${DESIGN}.v test_design/top_wrapper.v
-yosys -qp "tcl $SYNTH_TCL 4 top_wrapper test_design/${DESIGN}.json" ../generic/IO_1_bidirectional_frame_config_pass.v test_design/${DESIGN}.v test_design/top_wrapper.v
+yosys -qp "tcl $SYNTH_TCL 4 top_wrapper test_design/${DESIGN}.json" test_design/${DESIGN}.v test_design/top_wrapper.v
 
 FAB_ROOT=../../fabric_generator nextpnr-generic --uarch fabulous --json test_design/${DESIGN}.json -o fasm=test_design/${DESIGN}_des.fasm
 python3 ${BIT_GEN} -genBitstream test_design/${DESIGN}_des.fasm ../../fabric_generator/npnroutput/meta_data.txt test_design/${DESIGN}.bin

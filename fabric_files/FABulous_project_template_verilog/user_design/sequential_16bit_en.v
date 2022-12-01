b3830d80e70a036ca13fef998993c93e0c1b63f7
King Lok Chung <king.chung@student.manchester.ac.uk>
2022-12-01 18:27:07 +0000

    Fixing the bitstream command and update example design

--- fabric_files/FABulous_project_template_verilog/user_design/sequential_16bit_en.v
+++ fabric_files/FABulous_project_template_verilog/user_design/sequential_16bit_en.v
@@ -1,30 +1,17 @@
-module sequential_16bit_en (enable, reset, counter);
+module top(input wire clk, input wire [27:0] io_in, output wire [27:0] io_out, io_oeb);
+	wire rst = io_in[0];
+	wire en = io_in[1];
+	reg [15:0] ctr;
 
-input enable, reset;
-output reg [15:0] counter;
+	always @(posedge clk)
+		if (en)
+			if (rst)
+				ctr <= 0;
+			else
+				ctr <= ctr + 1'b1;
+		else
+			ctr <= ctr;
 
-wire clk;
-(* keep *) Global_Clock inst_clk (.CLK(clk));
-
-initial begin
-    counter = 16'b0000000000000000;
-end
-
-always @ (posedge clk) begin
-    if(enable) begin
-        if(reset) begin
-            counter <= 0;
-        end 
-        else begin
-            counter <= counter + 1'b1;
-        end
-    end
-end
-
-//assign {Tile_X0Y1_A_I,Tile_X0Y1_B_I,Tile_X0Y2_A_I,Tile_X0Y2_B_I,Tile_X0Y3_A_I,Tile_X0Y3_B_I,Tile_X0Y4_A_I,Tile_X0Y4_B_I,Tile_X0Y5_A_I,Tile_X0Y5_B_I,Tile_X0Y6_A_I,Tile_X0Y6_B_I,Tile_X0Y7_A_I,Tile_X0Y7_B_I,Tile_X0Y8_A_I,Tile_X0Y8_B_I} = counter;
-
+	assign io_out = {12'b0, ctr}; // pass thru reset for debugging
+	assign io_oeb = 28'b1;
 endmodule
-
-    //and_reg <= Tile_X0Y1_A_O & Tile_X0Y2_A_O;
-    //or_reg <= Tile_X0Y1_A_O | Tile_X0Y2_A_O;
-    //Tile_X0Y1_A_I <= and_reg ^ or_reg;

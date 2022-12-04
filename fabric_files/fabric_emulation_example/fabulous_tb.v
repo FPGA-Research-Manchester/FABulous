82c718785facd35208d61bf75f6484d068a6af89
Larry Ruckman <ruckman@slac.stanford.edu>
2022-12-04 15:08:52 -0800

    Update fabulous_tb.v
    ### Description
    - Bug fix to the enable the counter (active HIGH) after the reset pulse

--- fabric_files/fabric_emulation_example/fabulous_tb.v
+++ fabric_files/fabric_emulation_example/fabulous_tb.v
@@ -37,9 +37,9 @@ module fab_emulation_tb;
         $dumpvars(0, fab_emulation_tb);
 `endif
         repeat (100) @(posedge CLK);
-        O_top = 28'b1; // reset
+        O_top = 28'd1; // reset (rst = io_in[0])
         repeat (5) @(posedge CLK);
-        O_top = 28'b0;
+        O_top = 28'd2; // enable (en = io_in[1])
         for (i = 0; i < 100; i = i + 1) begin
             @(negedge CLK);
             $display("fabric = %b gold = %b", I_top, I_top_gold);
